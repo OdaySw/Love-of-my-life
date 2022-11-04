2db4317baf5aec41b3ca10cc1562197fdeec930f
OdaySw <113663005+OdaySw@users.noreply.github.com>
2022-11-04 21:24:47 +0200

    Add files via upload

---
+++ behav.js
@@ -0,0 +1,15 @@
+
+// Initial LOGIN Method
+function login() { 
+    var x = document.getElementById("User").value;
+    var y = document.getElementById("Pass").value;
+    
+    if((x==="Na3meah_alBuorito") && (y ==="17112020")) 
+    { window.open('index_Mp.html') }
+    else 
+    { alert("Wrong Password or User Name, Try again please!") }
+}
+//_____________________________________________________________
+//_____________________________________________________________
+//_____________________________________________________________
+
